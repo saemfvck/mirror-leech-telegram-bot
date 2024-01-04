a4882875bb3a2e4926975dcfdaf6ccd00f8a08dd
zeyn89 <128371500+ZeynDev@users.noreply.github.com>
2024-01-04 19:31:56 +0700

    New UI
    Menambahkan beberapa emoji:v
    ━━━━━━━━━━━━━━━━━━━━━━
    By : @ZeynDev (Eritsuu)

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -141,23 +141,24 @@ def get_readable_message(sid, is_user, page_no=1, status="All", page_step=1):
             MirrorStatus.STATUS_SEEDING,
             MirrorStatus.STATUS_SAMVID,
         ]:
-            msg += f"\n{get_progress_bar_string(task.progress())} {task.progress()}"
-            msg += f"\n<b>Processed:</b> {task.processed_bytes()} of {task.size()}"
-            msg += f"\n<b>Speed:</b> {task.speed()} | <b>ETA:</b> {task.eta()}"
+            msg += f"\n<b>┌┤</b>{get_progress_bar_string(task.progress())} {task.progress()}"
+            msg += f"\n<b>├ 🧤Processed:</b> {task.processed_bytes()} of {task.size()}"
+            msg += f"\n<b>├ 🧩Speed:</b> {task.speed()}"
+            msg += f"\n<b>├ ⏳ETA:</b> {task.eta()}"
             if hasattr(task, "seeders_num"):
                 try:
-                    msg += f"\n<b>Seeders:</b> {task.seeders_num()} | <b>Leechers:</b> {task.leechers_num()}"
+                    msg += f"\n<b>├ 🐾 Seeders:</b> {task.seeders_num()} | <b>🌿 Leechers:</b> {task.leechers_num()}"
                 except:
                     pass
         elif tstatus == MirrorStatus.STATUS_SEEDING:
-            msg += f"\n<b>Size: </b>{task.size()}"
-            msg += f"\n<b>Speed: </b>{task.seed_speed()}"
-            msg += f" | <b>Uploaded: </b>{task.uploaded_bytes()}"
-            msg += f"\n<b>Ratio: </b>{task.ratio()}"
-            msg += f" | <b>Time: </b>{task.seeding_time()}"
+            msg += f"\n<b>├ 🪁Size: </b>{task.size()}"
+            msg += f"\n<b>├ 🧩Speed: </b>{task.seed_speed()}"
+            msg += f"\n<b>├ 📤Uploaded: </b>{task.uploaded_bytes()}"
+            msg += f"\n<b>├ ⚙️Ratio: </b>{task.ratio()}"
+            msg += f"\n<b>├ ⏳Time: </b>{task.seeding_time()}"
         else:
-            msg += f"\n<b>Size: </b>{task.size()}"
-        msg += f"\n<code>/{BotCommands.CancelTaskCommand} {task.gid()}</code>\n\n"
+            msg += f"\n<b>├ 🪁Size: </b>{task.size()}"
+        msg += f"\n<b>└❌</b><code>/{BotCommands.CancelTaskCommand} {task.gid()}</code>\n\n"
 
     if len(msg) == 0 and status == "All":
         return None, None
@@ -165,7 +166,7 @@ def get_readable_message(sid, is_user, page_no=1, status="All", page_step=1):
         msg = f"No Active {status} Tasks!\n\n"
     buttons = ButtonMaker()
     if not is_user:
-        buttons.ibutton("📜", "status 0 ov", position="header")
+        buttons.ibutton("𖣘", "status 0 ov", position="header")
     if len(tasks) > STATUS_LIMIT:
         msg += f"<b>Page:</b> {page_no}/{pages} | <b>Tasks:</b> {tasks_no} | <b>Step:</b> {page_step}\n"
         buttons.ibutton("<<", f"status {sid} pre", position="header")
