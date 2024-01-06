816262d732a3b6a456e9ee6f82ba3088db7f873b
ZeynXD <128371500+ZeynDev@users.noreply.github.com>
2024-01-06 19:32:42 +0700

    Tes stats

--- bot/__main__.py
+++ bot/__main__.py
@@ -108,16 +108,15 @@ async def stats(_, message, edit_mode=False):
                 f"<b>┌ DISK:</b> {get_progress_bar_string(disk)}<code> {disk}%</code>\n" \
                 f"<b>└ Total:</b> <code>{total}</code> | <b>Free:</b> <code>{free}</code>"
 
- credit_stats = f"<blockquote><b>Credit </b>\n
-                 <b>Base Repo </b>\n
-                 <b>┌ Anas</b>\n
-                 <b>└ Github :</b>[Here](https://github.com/anasty17)\n\n
-                 <b>Edit & Modded </b>\n
-                 <b>┌ 𝐊𝐚𝐥𝐚𝐲𝐮𝐤𝐢-𝐅𝐞𝐥𝐢𝐜𝐞はなぶさ建設</b>\n
-                 <b>├ Github :</b>[Here](https://github.com/saemfvck)\n
-                 <b>├ ➤ 𝐄𝐫𝐢𝐭𝐬𝐮 𝐊𝐢𝐤𝐮𝐲𝐚</b>\n
-                 <b>└ Github :</b>[Here](https://github.com/ZeynDev)
-                 </blockquote>\n"
+ credit_stats = f"<b>Credit </b>\n" \
+                 f"<blockquote><b>Base Repo </b></blockquote>\n" \
+                 f"<b>┌ Anas</b>\n" \
+                 f"<b>└ Github :</b>[Here](https://github.com/anasty17)\n\n" \
+                 f"<blockquote><b>Edit & Modded </b></blockquote>\n" \
+                 f"<b>┌ 𝐊𝐚𝐥𝐚𝐲𝐮𝐤𝐢-𝐅𝐞𝐥𝐢𝐜𝐞はなぶさ建設</b>\n" \
+                 f"<b>├ Github :</b>[Here](https://github.com/saemfvck)\n" \
+                 f"<b>├ ➤ 𝐄𝐫𝐢𝐭𝐬𝐮 𝐊𝐢𝐤𝐮𝐲𝐚</b>\n" \
+                 f"<b>└ Github :</b>[Here](https://github.com/ZeynDev)\n"
               
 
     buttons.ibutton("Sys Stats",  "show_sys_stats")
